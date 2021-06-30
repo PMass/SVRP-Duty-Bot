1fe682ddf7a6cab7e11c9b797f34322c43a81ce5
PMass <parkermassie@gmail.com>
2021-06-30 13:06:42 -0700

    Update profile.js
    updated command to allow for it to use the message author if no one is specified.

--- commands/usermgmt/profile.js
+++ commands/usermgmt/profile.js
@@ -4,17 +4,16 @@ const dsGet = require('../../dsGet')
 
 module.exports = {
   commands: ['profile', 'info'],
-  minArgs: 1,
+  minArgs: 0,
   maxArgs: 1,
   expectedArgs: "<The target's @>",
   permissionError: 'You must be HR or higher to use this command.',
   permissions: 'VIEW_CHANNEL',
   callback: async (message, arguments) => {
     message.delete({ timeout: 5000 })
-    const mention = message.mentions.members.first()
+    var mention = message.mentions.members.first()
     if (!mention) {
-      message.reply('Please tag the user who you are putting into the database.')
-      return
+      mention = message.member
     }
     const [ userInfo, noMatch ] = await dbGet.userFull(message.guild.id, mention.id)
     if (noMatch) {
