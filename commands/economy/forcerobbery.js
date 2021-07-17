f3f9462407d842121159db87ad269ed023f59b61
PMass <parkermassie@gmail.com>
2021-07-16 18:02:34 -0700

    Update forcerobbery.js

--- commands/economy/forcerobbery.js
+++ commands/economy/forcerobbery.js
@@ -20,7 +20,7 @@ module.exports = {
       case "end":
         status = false
         await dbBattle.updtActive(message.guild.id, callType, status)
-        await battle.startBattle(guildID, name)
+        await battle.startBattle(guildID, callType)
         dsMsg.guildMessage(guild, `Closing and starting the ${callType} robbery for PD to repospond to`, message.channel.id, 30);
         break;
       default:
